cacf30f9fe2ed8408d93f626aa22e9e6e8870885
samyuktha495 <antagirisamyuktha@gmail.com>
2024-10-24 10:10:49 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,61 @@
+//for loop examples
+//post increment
+for(var a=1;a<=10;a++){
+console.log(a)
+}
+
+//post decrement
+for(var a=11;a>=6;a--){
+    console.log(a) 
+}
+
+//example2
+for(a=0;a<=5;a++){
+
+}
+console.log(a)
+
+//while loop examples
+//post increment
+var a=1;
+while(a<=5){
+    console.log(a)
+    a++
+}
+
+//post decrement
+var a=1;
+while(a<3){
+    console.log(a)
+    a++
+}
+
+//example2
+var a=1;
+while(a<=5){
+    console.log(a)
+    a++
+}
+
+//do-while loop examples
+//post increment
+var a=1;
+do{
+    a++
+    console.log(a)
+}while(a<=5)
+
+//post decrement
+var a=7;
+do{
+    a--
+    console.log(a)
+}while(a>=5)
+
+//example2
+var a=10;
+do{
+   
+    console.log(a)
+    a--
+}while(a>=7)
